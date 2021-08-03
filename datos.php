f26120e72d64bd5d66ede439e7157da49f47f7a4
Darman1987 <darman1987@hotmail.com>
2021-08-03 18:59:04 +1000

    crud_pdo

--- datos.php
+++ datos.php
@@ -3,47 +3,51 @@ header('Content-Type: application/json');
 
 require("conexion.php");
 
-$conexion = retornarConexion();
+$pdo = retornarConexion();
 
 switch ($_GET['accion']) {
     case 'listar':
-        $datos = mysqli_query($conexion, "select codigo,descripcion,precio from articulos");
-        $resultado = mysqli_fetch_all($datos, MYSQLI_ASSOC);
+        $sql = $pdo->prepare("select codigo,descripcion,precio from articulos");
+        $sql->execute();
+        $resultado = $sql->fetchAll(PDO::FETCH_ASSOC);
         echo json_encode($resultado);
         break;
 
     case 'agregar':
-        $descripcion = mysqli_real_escape_string($conexion, $_POST['descripcion']);
-        $precio = mysqli_real_escape_string($conexion, $_POST['precio']);
-        $respuesta = mysqli_query($conexion, "insert into articulos(descripcion,precio)
-                                                values ('$descripcion',$precio)");
-        echo json_encode($respuesta);
+        $sql = $pdo->prepare("insert into articulos(descripcion,precio) values (:descripcion,:precio)");
+        $resultado = $sql->execute(array(
+            "descripcion" => $_POST['descripcion'],
+            "precio" => $_POST['precio']
+        ));
+        echo json_encode($resultado);
         break;
 
     case 'borrar':
-        $codigo = mysqli_real_escape_string($conexion, $_GET['codigo']);
-        $respuesta = mysqli_query($conexion, "delete from articulos
-                                                where codigo=$codigo");
-        echo json_encode($respuesta);
+        $sql = $pdo->prepare("delete from articulos where codigo=:codigo");
+        $resultado = $sql->execute(array(
+            "codigo" => $_GET['codigo']
+        ));
+        echo json_encode($resultado);
         break;
 
     case 'consultar':
-        $codigo = mysqli_real_escape_string($conexion, $_GET['codigo']);
-        $datos = mysqli_query($conexion, "select codigo,descripcion,precio 
-                                            from articulos where codigo=$codigo");
-        $resultado = mysqli_fetch_all($datos, MYSQLI_ASSOC);
+        $sql = $pdo->prepare("select codigo,descripcion,precio from articulos where codigo=:codigo");
+        $sql->execute(array("codigo" => $_GET['codigo']));
+        $resultado = $sql->fetchAll(PDO::FETCH_ASSOC);
         echo json_encode($resultado);
         break;
 
     case 'modificar':
-        $descripcion = mysqli_real_escape_string($conexion, $_POST['descripcion']);
-        $precio = mysqli_real_escape_string($conexion, $_POST['precio']);
-        $codigo = mysqli_real_escape_string($conexion, $_GET['codigo']);
-        $respuesta = mysqli_query($conexion, "update articulos set
-                                                  descripcion='$descripcion',
-                                                  precio=$precio
-                                               where codigo=$codigo");
-        echo json_encode($respuesta);
+        $sql = $pdo->prepare("update articulos set
+                                  descripcion=:descripcion,
+                                  precio=:precio
+                                where codigo=:codigo");
+        $resultado = $sql->execute(array(
+            "descripcion" => $_POST['descripcion'],
+            "precio" => $_POST['precio'],
+            "codigo" => $_GET['codigo']
+        ));
+        echo json_encode($resultado);
         break;
 }
 ?>
